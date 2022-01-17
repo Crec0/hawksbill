a8401bfdc22a8bcaada13ae8003b77c4a2fe0887
Crec0 <83436716+Crec0@users.noreply.github.com>
2022-01-16 22:30:35 -0800

    kotlin build scripts

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    java
+    application
+    id("com.github.johnrengelman.shadow") version ("7.1.1")
+}
+
+val projectName = "MindBot"
+val botVersion = "1.0.0"
+val group = "club.mindtech"
+
+repositories {
+    mavenCentral()
+    maven {
+        name = "JDA"
+        url = uri("https://m2.dv8tion.net/releases")
+    }
+}
+
+dependencies {
+    implementation("net.dv8tion:JDA:5.+") {
+        exclude(module = "opus-java")
+    }
+    implementation("org.slf4j:slf4j-api:1.7.32")
+    implementation("ch.qos.logback:logback-classic:1.2.10")
+
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_16
+    targetCompatibility = JavaVersion.VERSION_16
+}
+
+application.apply {
+    mainClass.set("${group}.${projectName.toLowerCase()}.${projectName}")
+}
+
+tasks.named("shadowJar") {
+    doLast {
+        val jar = project.buildDir.resolve("libs").resolve("${projectName}-all.jar")
+        val renamedJar = project.buildDir.resolve("libs").resolve("${projectName}-${botVersion}.jar")
+        println("Renaming jar from $jar to $renamedJar")
+        jar.renameTo(renamedJar)
+    }
+}
