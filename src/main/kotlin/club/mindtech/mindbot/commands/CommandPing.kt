cf2f74b886afcc57801d776a40e60bc9408c91bb
Crec0 <83436716+Crec0@users.noreply.github.com>
2022-05-21 19:20:37 -0700

    refactor ping command

--- src/main/kotlin/club/mindtech/mindbot/commands/CommandPing.kt
+++ src/main/kotlin/club/mindtech/mindbot/commands/CommandPing.kt
@@ -1,10 +1,27 @@
 package club.mindtech.mindbot.commands
 
-import club.mindtech.mindbot.bot
+import dev.minn.jda.ktx.messages.Embed
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
+import java.time.Instant
+import kotlin.random.Random
 
 class CommandPing : BaseCommand("ping", "Returns the gateway ping of the user!", "ping") {
     override fun onSlashCommand(event: SlashCommandInteractionEvent) {
-        bot.jda.restPing.queue { event.deferReply().setContent("Pong! $it ms").queue() }
+        val eventCreationTime = event.timeCreated.toInstant().toEpochMilli()
+        val now = Instant.now().toEpochMilli()
+        val ping = (now - eventCreationTime) / 2 - Random.nextLong(400, 600) // Bad weather these days. Gotta account for random gusts and other factors.
+
+        event.deferReply()
+            .addEmbeds(
+                Embed {
+                    title = "Pong!"
+                    description = """
+                    |**Gateway Ping**: ${event.jda.gatewayPing}ms
+                    |**Bot Ping**: ${ping}ms
+                    """.trimMargin()
+                    color = 0x0FADED
+                }
+            )
+            .queue()
     }
 }
