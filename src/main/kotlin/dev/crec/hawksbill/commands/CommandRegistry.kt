abfb6564dd61e9cc9dc74fe90baa6320ca5d3851
Crec0 <83436716+Crec0@users.noreply.github.com>
2022-06-18 16:24:09 -0700

    smol funcs

--- src/main/kotlin/dev/crec/hawksbill/commands/CommandRegistry.kt
+++ src/main/kotlin/dev/crec/hawksbill/commands/CommandRegistry.kt
@@ -1,8 +1,6 @@
 package dev.crec.hawksbill.commands
 
 import dev.crec.hawksbill.commands.poll.CommandPoll
-import net.dv8tion.jda.api.interactions.commands.build.CommandData
-import okhttp3.internal.toImmutableMap
 
 private val COMMAND_MAP = setOf(
     CommandCalc(),
@@ -10,18 +8,10 @@ private val COMMAND_MAP = setOf(
     CommandPing(),
     CommandPoll(),
     CommandRemindMe(),
-).associateBy { it.name }.toImmutableMap()
+).associateBy { it.name }
 
 fun getRegisteredCommands() = COMMAND_MAP
 
-fun getSlashCommandData(): List<CommandData> {
-    return COMMAND_MAP.values.map { it.getCommandData() }
-}
+fun getSlashCommandData() = COMMAND_MAP.values.map { it.getCommandData() }
 
-fun getCommand(command: String?): BaseCommand? {
-    return if (command == null) {
-        null
-    } else {
-        COMMAND_MAP[command]
-    }
-}
+fun getCommand(command: String?) = if (command == null) null else COMMAND_MAP[command]
