0b3e4fcb510ef003a8fd2fb52bc14f16173b5f6a
Crec0 <83436716+Crec0@users.noreply.github.com>
2022-11-06 00:15:06 -0700

    Remove safe execution wrapper because of coroutine exception handler

--- src/main/kotlin/dev/crec/hawksbill/jda/EventListener.kt
+++ src/main/kotlin/dev/crec/hawksbill/jda/EventListener.kt
@@ -16,6 +16,25 @@ import net.dv8tion.jda.api.events.interaction.component.StringSelectInteractionE
 class EventListener: CoroutineEventListener {
     private val log = mainLogger.child("EventListener")
 
+    override suspend fun onEvent(event: GenericEvent) {
+        when (event) {
+            is CommandAutoCompleteInteractionEvent -> this.onAutoComplete(event)
+            is SlashCommandInteractionEvent -> this.onSlashCommand(event)
+            is GenericComponentInteractionCreateEvent -> this.onComponent(event)
+            is ModalInteractionEvent -> this.onModal(event)
+        }
+    }
+
+    private suspend fun onAutoComplete(event: CommandAutoCompleteInteractionEvent) {
+        log.debug("Executing autocomplete for ${event.name} invoked by ${event.user.name}")
+        bot.commands[event.name]!!.onAutoComplete(event)
+    }
+
+    private suspend fun onSlashCommand(event: SlashCommandInteractionEvent) {
+        log.debug("Executing command for ${event.name} invoked by ${event.user.name}")
+        bot.commands[event.name]!!.onSlashCommand(event)
+    }
+
     private suspend fun onComponent(event: GenericComponentInteractionCreateEvent) {
         val idArgs = event.componentId.split(":")
         val command = bot.commands[idArgs[0]]!!
@@ -30,43 +49,5 @@ class EventListener: CoroutineEventListener {
         val idArgs = event.modalId.split(":")
         val command = bot.commands[idArgs[0]]!!
         command.onModal(event, idArgs.subList(1, idArgs.size))
-    }
-
-    private suspend fun onSlashCommand(event: SlashCommandInteractionEvent) {
-        val message = "command for ${event.name} invoked by ${event.user.name}"
-        executeSilently("Failed to execute $message") {
-            log.info("Executing $message")
-            bot.commands[event.name]!!.onSlashCommand(event)
-        }
-    }
-
-    private suspend fun onAutoComplete(event: CommandAutoCompleteInteractionEvent) {
-        val message = "autocomplete for ${event.name} invoked by ${event.user.name}"
-        executeSilently("Failed to execute $message") {
-            log.info("Executing $message")
-            bot.commands[event.name]!!.onAutoComplete(event)
-        }
-    }
-
-    private inline fun executeSilently(errorMessage: String, invokable: () -> Unit) {
-        try {
-            invokable.invoke()
-        } catch (e: Exception) {
-            log.error(
-                """
-                $errorMessage: ${e.message}
-                ${e.stackTraceToString()}
-                """
-            )
-        }
-    }
-
-    override suspend fun onEvent(event: GenericEvent) {
-        when (event) {
-            is ModalInteractionEvent -> this.onModal(event)
-            is SlashCommandInteractionEvent -> this.onSlashCommand(event)
-            is CommandAutoCompleteInteractionEvent -> this.onAutoComplete(event)
-            is GenericComponentInteractionCreateEvent -> this.onComponent(event)
-        }
     }
 }
