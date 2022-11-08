adda032d7376d096f5c38180bfb4b17f5a1771f6
Crec0 <83436716+Crec0@users.noreply.github.com>
2022-11-07 20:34:39 -0800

    Add Easter egg in Ping command

--- src/main/kotlin/dev/crec/hawksbill/impl/commands/CommandPing.kt
+++ src/main/kotlin/dev/crec/hawksbill/impl/commands/CommandPing.kt
@@ -2,10 +2,10 @@ package dev.crec.hawksbill.impl.commands
 
 import dev.crec.hawksbill.api.annotation.SlashCommandMarker
 import dev.crec.hawksbill.api.command.ICommand
+import dev.crec.hawksbill.bot
 import dev.crec.hawksbill.utility.extensions.alignStringPair
 import dev.minn.jda.ktx.interactions.commands.Command
 import dev.minn.jda.ktx.messages.Embed
-import net.dv8tion.jda.api.entities.Message.MentionType
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
 import net.dv8tion.jda.api.utils.MarkdownUtil
 import kotlin.random.Random
@@ -19,40 +19,34 @@ class CommandPing : ICommand {
     )
 
     override suspend fun onSlashCommand(event: SlashCommandInteractionEvent) {
-
-        val fields = listOf(
-            "Gateway" to event.jda.gatewayPing,
-            "Bot" to Random.nextInt(100, 200),
-        )
-        val maxLength = fields.maxOfOrNull { it.first }!!.length
-
-        val embedDescription = fields
-            .map { it.first to it.second.toString() }
-            .map { alignStringPair(it, maxLength - it.first.length) }
-            .joinToString("") { MarkdownUtil.codeblock(it) }
-
         val isEasterEggPing = Random.nextInt(20) == 0
 
         val embed = if (isEasterEggPing) {
             Embed {
                 title = "Pong!"
-                description = embedDescription
+                image = bot.tenorAPI.getRandomGifUrl("discord ping")
                 color = 0x0FADED
             }
         } else {
+            val fields = listOf(
+                "Gateway" to event.jda.gatewayPing,
+                "Bot" to Random.nextInt(100, 200),
+            )
+            val maxLength = fields.maxOfOrNull { it.first }!!.length
+            val body = fields
+                .map { it.first to it.second.toString() }
+                .map { alignStringPair(it, maxLength - it.first.length) }
+                .joinToString("") { MarkdownUtil.codeblock(it) }
+
             Embed {
                 title = "Pong!"
-                description = embedDescription
+                description = body
                 color = 0x0FADED
             }
         }
 
-        val reply = event.deferReply().addEmbeds(embed)
-
-        if (isEasterEggPing) {
-            reply.allowedMentions.add(MentionType.USER)
-        }
-
-        reply.queue()
+        event.deferReply()
+            .addEmbeds(embed)
+            .queue()
     }
 }
